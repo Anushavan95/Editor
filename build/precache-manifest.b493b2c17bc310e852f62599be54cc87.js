b26b01bc6a2dafa34fb23c77a4f0e1923f9bc0f5
Anushavan95 <yanushavan@gmail.com>
2022-02-09 15:48:51 +0400

    commit gh

---
+++ build/precache-manifest.b493b2c17bc310e852f62599be54cc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64f64297ea9a7fda23e",
+    "url": "/Anushavan95/EditorBuilder/static/css/main.d33fb995.chunk.css"
+  },
+  {
+    "revision": "d64f64297ea9a7fda23e",
+    "url": "/Anushavan95/EditorBuilder/static/js/main.abc960ca.chunk.js"
+  },
+  {
+    "revision": "cb8c1160cf18bdd28409",
+    "url": "/Anushavan95/EditorBuilder/static/js/runtime~main.7ae431f3.js"
+  },
+  {
+    "revision": "29688f4ee5a98cb3b59a",
+    "url": "/Anushavan95/EditorBuilder/static/js/2.bb1e7362.chunk.js"
+  },
+  {
+    "revision": "a626a729842576d6436a546152bce31e",
+    "url": "/Anushavan95/EditorBuilder/static/media/link.a626a729.png"
+  },
+  {
+    "revision": "6042fd51ae693a8be3b316d8c2409c70",
+    "url": "/Anushavan95/EditorBuilder/index.html"
+  }
+];
