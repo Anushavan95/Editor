354ca132760b202c26091e75357466b36b673bbc
Anushavan95 <yanushavan@gmail.com>
2022-02-21 10:42:15 +0400

    image default

--- src/Components/EditorBuilder/ComponentsEditor/Heading.jsx
+++ src/Components/EditorBuilder/ComponentsEditor/Heading.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
-function Heading({tagEntry, handleClick, componentData, styles}) {
-    return (
-        <>
-            {/*{"id" + componentData.id}*/}
-            <div
-                style={styles}
-                id={componentData.id}
-                onClick={() => handleClick(componentData.id)}
-                contentEditable={true}
-                dangerouslySetInnerHTML={{
-                    __html: tagEntry
-                }}
-            />
-        </>
-    );
+function Heading({ tagEntry, handleClick, componentData, styles }) {
+  return (
+    <>
+      <div
+        style={styles}
+        id={componentData.id}
+        onClick={() => handleClick(componentData.id)}
+        contentEditable={true}
+        dangerouslySetInnerHTML={{
+          __html: tagEntry
+        }}
+      />
+    </>
+  );
 }
 
 export default Heading;
--- src/Components/EditorBuilder/ComponentsEditor/ImageUploading.jsx
+++ src/Components/EditorBuilder/ComponentsEditor/ImageUploading.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import PlaceHolderImage from "../../../images/placeholder.png";
 
 export default function ImageUploadingApp({ image }) {
+  // const srcImage = image.length > 0 ? image : PlaceHolderImage;
+  const srcImage = image.length > 0 ? image : PlaceHolderImage;
+  const imageStyles = {
+    width: "100%"
+  };
   return (
     <div>
       <div>
-        <div>
-          <img src={image} />
-          <h1>Select Image</h1>
-        </div>
+        <img src={srcImage} alt="Image_Upload" style={imageStyles} />
       </div>
     </div>
   );
