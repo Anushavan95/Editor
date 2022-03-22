31296a2dfcbd07f9b4b87ea1d244b8ff1780d522
Anushavan95 <yanushavan@gmail.com>
2022-03-22 15:21:31 +0400

    Updates

---
+++ precache-manifest.7dd520c8776d939bfce2b88e67070f09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7345732f42cec568948",
+    "url": "/Anushavan95/Editor/static/css/main.d533752f.chunk.css"
+  },
+  {
+    "revision": "f7345732f42cec568948",
+    "url": "/Anushavan95/Editor/static/js/main.409aab14.chunk.js"
+  },
+  {
+    "revision": "b13edbbe4684d7120a29",
+    "url": "/Anushavan95/Editor/static/js/runtime~main.5ad3045c.js"
+  },
+  {
+    "revision": "13e63af42cefab29c9f4",
+    "url": "/Anushavan95/Editor/static/js/2.be1104ee.chunk.js"
+  },
+  {
+    "revision": "2bcad987b5550fe87fac4c3d467e8644",
+    "url": "/Anushavan95/Editor/static/media/Editor.2bcad987.svg"
+  },
+  {
+    "revision": "9c2831c4d2b23c4506ecb4b316dd26ff",
+    "url": "/Anushavan95/Editor/static/media/heading.9c2831c4.svg"
+  },
+  {
+    "revision": "cb515ad5c3ed9000e0e8c975f90c6bb0",
+    "url": "/Anushavan95/Editor/static/media/HyperLink.cb515ad5.svg"
+  },
+  {
+    "revision": "c2881a77c9cfda6998bc7aece2c5ccd2",
+    "url": "/Anushavan95/Editor/static/media/image.c2881a77.svg"
+  },
+  {
+    "revision": "4293e9aa4afce0578c59cdc16207348f",
+    "url": "/Anushavan95/Editor/static/media/imagesLink.4293e9aa.svg"
+  },
+  {
+    "revision": "888c3ced96dc118703e5823739d32abd",
+    "url": "/Anushavan95/Editor/static/media/accordionFAQ.888c3ced.svg"
+  },
+  {
+    "revision": "f5fcd803f288042fcb75bf21e1038f83",
+    "url": "/Anushavan95/Editor/index.html"
+  }
+];
